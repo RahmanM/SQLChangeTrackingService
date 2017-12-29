2ab089a53f94ca859be84ec868d54067c631a262
Rahman Mahmoodi <rahman.mahmoodi@gmail.com>
2017-12-29 19:34:11 +1100

    clean up of client and channel factory

--- Sql.ChangeTracking.Client/Program.cs
+++ Sql.ChangeTracking.Client/Program.cs
@@ -1,12 +1,8 @@
 ﻿using Polly;
 using Sql.ChangeTracking.Common;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Sql.ChangeTracking.Client
 {
@@ -21,135 +17,58 @@ namespace Sql.ChangeTracking.Client
             try
             {
                 Policy.Handle<Exception>()
-               .RetryForever()
-               .Execute(() => ConnectToService());
+                   .RetryForever()
+                   .Execute(() =>
+                   {
+                       Execute();
+                   }
+                );
             }
             catch (Exception ex)
             {
-
-                Console.WriteLine(ex.Message); ;
+                Console.WriteLine(ex.Message);
             }
 
             Console.Read();
         }
 
-        private static void ConnectToService()
+        private static void Execute()
         {
-            DuplexChannelFactory<IChangeTrackingSubscriptions> channelFactory = null;
+            var client = new DuplexChannelFactoryClient<IChangeTrackingSubscriptions, IEventNotificationCallback>
+                (new CallbackCleint(), "net.tcp://localhost:9002/Sql.ChangeTracking.Wcf/wcf/SqlChangeTrackingWcfService");
 
-            try
-            {
-                // Basic A, B of the wcf service
-                BasicHttpBinding binding = new BasicHttpBinding();
-                //EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:9002/Sql.ChangeTracking.Wcf/Wcf/");
-                EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:9002/Sql.ChangeTracking.Wcf/wcf/SqlChangeTrackingWcfService");
+            var channel = client.CreateChannel();
+            ((ICommunicationObject) channel).Opened += Channel_Opned;
+            ((ICommunicationObject) channel).Closed += Channel_Closed;
+            ((ICommunicationObject) channel).Faulted += Channel_Faulted;
 
-                // Pass Binding and EndPoint address to ChannelFactory, NB: it has to be DuplexChannel as we expect to be called back 
-                channelFactory =
-                  new DuplexChannelFactory<IChangeTrackingSubscriptions>(
-                      new InstanceContext(new CallbackCleint()),
-                      new NetTcpBinding(),
-                      endpointAddress);
-
-                channelFactory.Faulted += ChannelFactory_Faulted;
-                channelFactory.Closed += ChannelFactory_Closed;
-                channelFactory.Opened += ChannelFactory_Opened;
-
-                IChangeTrackingSubscriptions channel = channelFactory.CreateChannel();
-
-                ((ICommunicationObject)channel).Opened += Channel_Opned;
-                ((ICommunicationObject)channel).Closed += Channel_Closed;
-                ((ICommunicationObject)channel).Faulted += Channel_Faulted;
-
-                // Subscribe as "Client1" and wait for changes to "Customer" table
-                channel.Subscribe("Client1", "Customer");
-                channel.Subscribe("Client1", "Employee");
-
-                Console.WriteLine("To unsubscribe press, 'u'");
-                var input = Console.ReadLine();
-                if (input == "u")
-                {
-                    channel.Unsubscribe("Client1", "Customer");
-                    Console.WriteLine("Unsubscribed!");
-                }
-
-            }
-            catch (TimeoutException ex)
-            {
-                //Timeout error  
-                if (channelFactory != null)
-                    channelFactory.Abort();
-
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            catch (FaultException ex)
-            {
-                if (channelFactory != null)
-                    channelFactory.Abort();
-
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            catch (CommunicationException ex)
-            {
-                //Communication error  
-                if (channelFactory != null)
-                    channelFactory.Abort();
-
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-            catch (Exception ex)
-            {
-                if (channelFactory != null)
-                    channelFactory.Abort();
-
-                Console.WriteLine(ex.Message);
-                throw;
-            }
+            // Subscribe as "Client1" and wait for changes to "Customer" table
+            channel.Subscribe("Client1", "Customer");
+            channel.Subscribe("Client1", "Employee");
+           
         }
 
         private static void Channel_Faulted(object sender, EventArgs e)
         {
-            Console.WriteLine("Channel is Faulted.");
-            HandleExceptionAndRetry(ConnectToService);
-        }
-
-     
-        private static void HandleExceptionAndRetry(Action action)
-        {
-            // Todo: use polly etc
-            Thread.Sleep(1000);
-            action();
+            Console.WriteLine("Channel Faulted");
+            Execute();
         }
 
         private static void Channel_Closed(object sender, EventArgs e)
         {
-            Console.WriteLine("Channel is closed.");
-            HandleExceptionAndRetry(ConnectToService);
+            Console.WriteLine("Channel Closed");
+            Execute();
         }
 
         private static void Channel_Opned(object sender, EventArgs e)
         {
-            Console.WriteLine("Channel is opned.");
+            Console.WriteLine($"Channel Opened {DateTime.Now}");
         }
 
-        private static void ChannelFactory_Opened(object sender, EventArgs e)
+        private void HandleExceptionAndRetry(Func<ICommunicationObject> connectToService)
         {
-            Console.WriteLine("Channel Factory is Opened.");
-        }
-
-        private static void ChannelFactory_Closed(object sender, EventArgs e)
-        {
-            Console.WriteLine("Channel Factory is closed.");
-            HandleExceptionAndRetry(ConnectToService);
-        }
-
-        private static void ChannelFactory_Faulted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Channel is Faulted.");
-            HandleExceptionAndRetry(ConnectToService);
+            Thread.Sleep(1000);
+            connectToService();
         }
     }
 
@@ -162,7 +81,7 @@ namespace Sql.ChangeTracking.Client
             Console.WriteLine("Disposed");
         }
 
-        public  void PublishTableChange(string tableName)
+        public void PublishTableChange(string tableName)
         {
             Counter++;
             Console.WriteLine($"{DateTime.Now} - Table {tableName} was changed. Total messages: {Counter}");
---
+++ Sql.ChangeTracking.Common/WcfClient/DuplexChannelFactoryClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading;
+
+namespace Sql.ChangeTracking.Client
+{
+    public class DuplexChannelFactoryClient<T,U> 
+        where T : class
+        where U : class
+    {
+
+        public DuplexChannelFactoryClient(U callback, string serviceAddress)
+        {
+            if (callback == null)
+                throw new ArgumentNullException(nameof(callback));
+
+            Callback = callback;
+            ServiceAddress = serviceAddress;
+        }
+
+        public U Callback { get; set; }
+        public string ServiceAddress { get; set; }
+
+        public T CreateChannel()
+        {
+
+            DuplexChannelFactory<T> channelFactory = null;
+
+            try
+            {
+
+                // Basic A, B of the wcf service
+                var binding = new BasicHttpBinding();
+                var endpointAddress = new EndpointAddress(ServiceAddress);
+
+                // Pass Binding and EndPoint address to ChannelFactory, NB: it has to be DuplexChannel as we expect to be called back 
+                channelFactory =
+                  new DuplexChannelFactory<T>(
+                      new InstanceContext(Callback),
+                      new NetTcpBinding(),
+                      endpointAddress);
+
+                channelFactory.Faulted += ChannelFactory_Faulted;
+                channelFactory.Closed += ChannelFactory_Closed;
+
+                T channel = channelFactory.CreateChannel();
+                return channel;
+
+            }
+            catch (TimeoutException ex)
+            {
+                //Timeout error  
+                if (channelFactory != null)
+                    channelFactory.Abort();
+
+                throw;
+            }
+            catch (FaultException ex)
+            {
+                if (channelFactory != null)
+                    channelFactory.Abort();
+
+                throw;
+            }
+            catch (CommunicationException ex)
+            {
+                //Communication error  
+                if (channelFactory != null)
+                    channelFactory.Abort();
+
+                throw;
+            }
+            catch (Exception ex)
+            {
+                if (channelFactory != null)
+                    channelFactory.Abort();
+
+                throw;
+            }
+        }
+            
+        private void ChannelFactory_Closed(object sender, EventArgs e)
+        {
+            HandleExceptionAndRetry(CreateChannel);
+        }
+
+        private void ChannelFactory_Faulted(object sender, EventArgs e)
+        {
+            HandleExceptionAndRetry(CreateChannel);
+        }
+
+        private void HandleExceptionAndRetry(Func<T> connectToService)
+        {
+            // TODO: use polly or something
+            Thread.Sleep(1000);
+            connectToService();
+        }
+    }
+}
